7755191f7107bb1d69df2aeda6a252b6b2eab8e8
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2024-11-02 14:07:38 +0530

    do not use this updated code has it is not compeleted.

--- setup.py
+++ setup.py
@@ -15,12 +15,21 @@ def main():
 
         print("The current directory is:", current_dir)
         print("add this path to your zshrc if your are following the optional steps. ---> ",current_dir+"/code/key.py")
-
+        with open(current_dir+"/json files/export.json", 'r') as file:
+           key_bind = json.load(file)
 
         keyboard_binding_in1=input(f"{colorama.Fore.GREEN}which keys should be used for activate the text mode . right the key name with a comma . example:control,shift. --->  ")
         short_key1=keyboard_binding_in1.split(",")
-
+        print(key_bind["key_binding1"])
         print(f"{colorama.Fore.WHITE}{short_key1[0],short_key1[1]}")
+        key_bind["key_binding1"]=[]
+        with open(current_dir+"/json files/export.json", 'w') as file:
+            json.dump(key_bind, file)
+        key_bind["key_binding1"].append(short_key1[0])
+        key_bind["key_binding1"].append(short_key1[1])
+
+        with open(current_dir+"/json files/export.json", 'w') as file:
+            json.dump(key_bind, file)
 
 
 if __name__ == "__main__":
