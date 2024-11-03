c0d1fd424bb4dd605f8186be29be46f4c16ee52a
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2024-11-03 09:38:17 +0530

    do not use this updated code has it is not compeleted.

--- setup.py
+++ setup.py
@@ -17,6 +17,10 @@ def main():
         print("add this path to your zshrc if your are following the optional steps. ---> ",current_dir+"/code/key.py")
         with open(current_dir+"/json files/config.json", 'r') as file:
            key_bind = json.load(file)
+        in1=input(f"{colorama.Fore.GREEN}Do need help --->  ")
+        if "y" in in1:
+            print("keys  : [shift , control , option , command , tab , delete , capslock , escape , enter , end , home]")
+
 
         keyboard_binding_in1=input(f"{colorama.Fore.GREEN}which keys should be used for activate the text mode . right the key name with a comma . example:control,shift. --->  ")
         short_key1=keyboard_binding_in1.split(",")
