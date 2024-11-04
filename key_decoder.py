a1368a9d9cfd72ce5da439ec42dbb77781e59b63
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2024-11-04 08:32:33 +0530

    do not use this updated code has it is not compeleted.

--- key_decoder.py
+++ key_decoder.py
@@ -1,4 +1,34 @@
 import json
-from setup import keyslist
-print(keyslist)
-check_list=["delete","coomand","option","escape","capslock","f1","f2","f3","f4","f5","f6","f7","f8","f9","f10","f11","f12"]
+import os
+import threading
+from setup import string_print
+current_file_path = os.path.abspath(__file__)
+current_dir = os.path.dirname(current_file_path)
+
+
+check_list=["delete","command","option","escape","capslock","f1","f2","f3","f4","f5","f6","f7","f8","f9","f10","f11","f12"]
+with open(current_dir+"/json files/config.json", 'r') as file:
+  key_list = json.load(file)
+
+
+
+
+
+def m1(list,index):
+   for i in range(len(check_list)):
+        if check_list[i] in key_list[f"{list}"][index]:
+            string_print(f"true{index,list}",0.02,"")
+            if index ==0:
+                key_coder=[key_list[f"{list}"][index],key_list[f"{list}"][1]]
+                print(key_coder)
+            if index ==1:
+                key_coder=[key_list[f"{list}"][0],key_list[f"{list}"][index]]
+                print(key_coder)
+
+def t1(list_):
+    for i in range(len(key_list[f"{list_}"])):
+        thread = threading.Thread(target=m1(f"{list_}",i))
+        thread.start()
+
+t1("key_binding1")
+t1("key_binding2")
--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ def main():
         current_dir = os.path.dirname(current_file_path)
 
 
-        string_print("The current directory is:"+current_dir,0.02,"")
+        string_print("The current directory is:"+current_dir,0.01,"")
         print("  ")
-        string_print("add this path to your zshrc if your are following the optional steps. ---> "+current_dir+"/code/key.py",0.02,"")
+        string_print("add this path to your zshrc if your are following the optional steps. ---> "+current_dir+"/code/key.py",0.01,"")
         print("  ")
 
 
@@ -36,12 +36,12 @@ def main():
            key_bind = json.load(file)
 
 
-        string_print("Do need help --->",0.02,colorama.Fore.MAGENTA)
+        string_print("Do need help --->",0.01,colorama.Fore.MAGENTA)
         in1=input(f"{colorama.Fore.GREEN} ")
         print("__")
         if "y" in in1:
             string_0="keys  : [shift , control , option , command , tab , delete , capslock , escape , enter , end , home , f1 , f2 , f3 ,f4 , f5 , f6 , f7 , f8 , f9 , f10 , f11 , f12]"
-            string_print(string_0,0.02,colorama.Fore.WHITE)
+            string_print(string_0,0.01,colorama.Fore.WHITE)
         print("")
         print(f"{colorama.Fore.GREEN}__")
 
@@ -50,7 +50,7 @@ def main():
         try:
 
             string_1="which keys should be used for activating the text mode . type the key name with a comma . example:shift,control.  ---> "
-            string_print(string_1,0.04,colorama.Fore.GREEN)
+            string_print(string_1,0.02,colorama.Fore.GREEN)
 
             keyboard_binding_in1=input(f"{colorama.Fore.BLUE} ")
 
@@ -67,7 +67,7 @@ def main():
 #voice
             print("  ")
             string_2="which keys should be used for activating the voice mode . type the key name with a comma . example: shift,f1.  ---> "
-            string_print(string_2,0.04,colorama.Fore.BLUE)
+            string_print(string_2,0.02,colorama.Fore.BLUE)
             keyboard_binding_in2=input(f"{colorama.Fore.MAGENTA} ")
 
             short_key2=keyboard_binding_in2.split(",")
