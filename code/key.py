d69645ecc607b427ae91de0ac952a2302de1fac4
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2024-11-17 16:43:55 +0530

    do not use this code as this is a test for ML

--- code/key.py
+++ code/key.py
@@ -186,11 +186,7 @@ def detect(text,data,mode):
 
 
                 except:
-                    if is_connected:
-                        print("")
-                        string_print("Error:missing value",0.02,Fore.RED)
-                        print("")
-                        print("No matching question found.")
+                   
                         if mode==False:
                             subprocess.call(["say", "-v", "Daniel", "No matching question found."])
                             learn_fun("should I start the machine learning protocol?",["no","start"],text.lower())
@@ -201,9 +197,7 @@ def detect(text,data,mode):
                             a=str(a)
                             a=a.lower()
                             text_learn(text.lower(),a)
-                    else:
-                        print("")
-                        string_print("check if your are conected to the internet 🛜",0.02,Fore.RED)
+                    
 
 
      # elif "understand" in text.lower() or "learn" in text.lower():
